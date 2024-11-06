68b65c03848140de480e95e698f29d377b311e29
Dereje Getachew <101693929+derejegetachew@users.noreply.github.com>
2024-11-06 16:56:49 +0300

    create  table with route and query

--- app.js
+++ app.js
@@ -23,3 +23,63 @@ mysqlconnection.connect((error) => {
     console.log("Connection successful");
   }
 });
+app.get("/install", (req, res) => {
+  const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS products (
+      product_id INT AUTO_INCREMENT,
+      product_url VARCHAR(67) NOT NULL,
+      product_name VARCHAR(78) NOT NULL,
+      PRIMARY KEY (product_id)
+    )
+  `;
+
+  mysqlconnection.query(createTableQuery, (error, result) => {
+    if (error) {
+      console.error("Error creating table:", error);
+      res.status(500).send("Error creating table");
+    } else {
+      console.log("Table created successfully");
+      res.send("Table created successfully");
+    }
+  });
+
+  const createProductDescription = `create table if not exists productDescription(
+descrption_id int auto_increment,
+product_id int not null,
+product_brief_description varchar(255) not null,
+product_link varchar(255) not null,
+primary key(descrption_id),
+foreign key (product_id) references products(product_id )
+)`;
+
+  mysqlconnection.query(createProductDescription, (error, result) => {
+    if (error) {
+      console.error("Error creating table:", error);
+      res.status(500).send("Error creating table");
+    } else {
+      console.log("Table created successfully");
+      res.send("Table created successfully");
+    }
+  });
+
+  const createProductPrice = `
+  CREATE TABLE IF NOT EXISTS productPrice (
+    price_id INT AUTO_INCREMENT,
+    product_id INT NOT NULL,
+    starting_price VARCHAR(255) NOT NULL,
+    price_range VARCHAR(255) NOT NULL,
+    PRIMARY KEY (price_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+  )
+`;
+
+  mysqlconnection.query(createProductPrice, (error, result) => {
+    if (error) {
+      console.error("Error creating table:", error);
+      res.status(500).send("Error creating table");
+    } else {
+      console.log("Table created successfully");
+      res.send("Table created successfully");
+    }
+  });
+});
